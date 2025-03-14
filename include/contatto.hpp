1c239d576749bae7c13bccbd5e5a9f238a16390d
MattiaP7 <7mattiapirazzi@gmail.com>
2025-03-14 22:50:15 +0100

    Documentation Update.

--- include/contatto.hpp
+++ include/contatto.hpp
@@ -5,6 +5,10 @@
 #pragma once
 #include <string>
 
+/**
+ * @brief Classe `Contatto`, rappresenta il contatto nella rubrica
+ *
+ */
 class Contatto {
 private:
     std::string nome_completo;
@@ -12,19 +16,39 @@ private:
     std::string email;
 
 public:
+    /// @brief costruttore di default
     Contatto();
 
+    /// @brief metodo per inserire i valori del contatto
     void inserisci();
 
+    /// @brief metodo per stampare i dati di un contatto
     void print() const;
 
+    /// @brief metodo che permette la modifica dei dati di un contatto tramite un menu di scelta
     void modifica();
+
+    // tutti i getter, servono a ritornare i dati effettivi del contatto
 
     const std::string& get_nome() const;
     const std::string& get_telefono() const;
     const std::string& get_email() const;
 
+    /**
+     * @brief metodo che ritorna una stirnga costituita dai dati del contatto
+     *
+     * @return std::string
+     */
     std::string to_csv() const;
+
+    /**
+     * @brief metodo che carica i dati da un file CSV e gli inserisci in una struttura dati
+     * 1 - Legge il file `contatti.csv`
+     * 2 - Estraee i dati
+     * 3 - Crea oggetti "Contatto" e gli aggiunge alla struttura dati
+     *
+     * @param line
+     */
     void from_csv(const std::string& line);
 };
 
--- include/list.hpp
+++ include/list.hpp
@@ -4,6 +4,10 @@
 #pragma once
 #include "contatto.hpp"
 
+/**
+ * @brief strutta per rappresentare un nodo della lista
+ *
+ */
 struct Nodo {
     Contatto contatto;
     Nodo* next;
@@ -11,23 +15,53 @@ struct Nodo {
     Nodo();
 };
 
+/**
+ * @brief classe List
+ *
+ */
 class List {
 private:
-    Nodo* head;
+    Nodo* head; // puntatore alla testa della lista
+
+    /**
+     * @brief quando si crea/elimina un contatto viene chiamato questo metodo che aggiorna il file .csv
+     *
+     */
     void salva_su_file() const;
-    size_t M_size = 0;
+    size_t M_size = 0; // tiene traccia della grandezza della lisat
 
 public:
+    /// @brief costruttore di default
     List();
+    /// @brief distruttore di default
     ~List();
 
+    /// @brief metodo per l'inserimento in testa di un nodo nella lista, verifica che non ci siano nodi uguali e fa un inserimento ordinato
     void push();
+
+    /// @brief metodo che mostra tutti i contatti della rubrica
     void mostra() const;
+
+    /// @brief metodo per cercare un contatto nella rubrica
     void find() const;
+
+    /// @brief metodo per eliminare un contatto della rubrica
     void erase();
+
+    /// @brief metodo che elimina tutti i contatti uguali
     void unique();
+
+    /// @brief metodo per ritornare la dimensione della lista
+    /// @return size_t - grandezza della lista
     constexpr size_t get_size();
+
+    /// @brief metodo per modifica un conttatto della rubrica
     void modifica();
+
+    /**
+     * @brief metodo che carica i dati del file csv alla lista
+     *
+     */
     void carica_da_file();
 };
 
